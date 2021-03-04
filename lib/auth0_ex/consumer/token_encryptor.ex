583413e017f6002e6afbc91c9541a442234d8c98
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-03-04 14:48:42 +0100

    Properly retrieve encryption key from config

--- lib/auth0_ex/consumer/token_encryptor.ex
+++ lib/auth0_ex/consumer/token_encryptor.ex
@@ -1,10 +1,9 @@
 defmodule Auth0Ex.Consumer.TokenEncryptor do
-  @token_encryption_key Application.fetch_env!(:auth0_ex, :token_encryption_key) |> Base.decode64!()
   @aad "AES256GCM"
 
   def encrypt(plaintext) do
     iv = :crypto.strong_rand_bytes(16)
-    {ciphertext, tag} = :crypto.crypto_one_time_aead(:aes_256_gcm, @token_encryption_key, iv, plaintext, @aad, true)
+    {ciphertext, tag} = :crypto.crypto_one_time_aead(:aes_256_gcm, token_encryption_key(), iv, plaintext, @aad, true)
 
     encrypted = iv <> tag <> ciphertext
     Base.encode64(encrypted)
@@ -13,6 +12,11 @@ defmodule Auth0Ex.Consumer.TokenEncryptor do
   def decrypt(encrypted) do
     <<iv::binary-16, tag::binary-16, ciphertext::binary>> = Base.decode64!(encrypted)
 
-    :crypto.crypto_one_time_aead(:aes_256_gcm, @token_encryption_key, iv, ciphertext, @aad, tag, false)
+    :crypto.crypto_one_time_aead(:aes_256_gcm, token_encryption_key(), iv, ciphertext, @aad, tag, false)
+  end
+
+  defp token_encryption_key do
+    encoded_key = Application.fetch_env!(:auth0_ex, :token_encryption_key)
+    Base.decode64!(encoded_key)
   end
 end
