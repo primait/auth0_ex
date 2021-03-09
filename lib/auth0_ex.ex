8750f94eba9927282d4ed0d6bf02a5c606bc2cc8
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-03-09 10:16:44 +0100

    Add token verification to Auth0Ex entrypoint

--- lib/auth0_ex.ex
+++ lib/auth0_ex.ex
@@ -1,7 +1,11 @@
 defmodule Auth0Ex do
-  alias Auth0Ex.TokenProvider
+  alias Auth0Ex.{Token, TokenProvider}
 
   def token_for(audience) do
     TokenProvider.token_for(TokenProvider, audience)
+  end
+
+  def verify_and_validate(token) do
+    Token.verify_and_validate(token)
   end
 end
