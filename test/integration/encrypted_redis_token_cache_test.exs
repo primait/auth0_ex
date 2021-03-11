62e5d537e98adf6032d3de9655ced3eac8156794
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-03-11 14:13:50 +0100

    Extract redis integration test audience to constant

--- test/integration/encrypted_redis_token_cache_test.exs
+++ test/integration/encrypted_redis_token_cache_test.exs
@@ -5,29 +5,30 @@ defmodule Auth0Ex.TokenProvider.EncryptedRedisTokenCacheTest do
   alias Auth0Ex.TokenProvider.{EncryptedRedisTokenCache, TokenInfo}
 
   @namespace Application.compile_env!(:auth0_ex, :cache)[:namespace]
+  @test_audience "redis-integration-test-audience"
 
   setup do
     redis_connection_uri = Application.fetch_env!(:auth0_ex, :cache)[:redis_connection_uri]
     Redix.start_link(redis_connection_uri, name: :redix)
-    Redix.command!(:redix, ["DEL", token_key("audience")])
+    Redix.command!(:redix, ["DEL", token_key(@test_audience)])
 
     :ok
   end
 
   test "persists and retrieves tokens" do
-    :ok = EncryptedRedisTokenCache.set_token_for("audience", sample_token())
+    :ok = EncryptedRedisTokenCache.set_token_for(@test_audience, sample_token())
 
-    assert {:ok, sample_token()} == EncryptedRedisTokenCache.get_token_for("audience")
+    assert {:ok, sample_token()} == EncryptedRedisTokenCache.get_token_for(@test_audience)
   end
 
   test "returns {:ok, nil} when token is not cached" do
-    assert {:ok, nil} == EncryptedRedisTokenCache.get_token_for("audience")
+    assert {:ok, nil} == EncryptedRedisTokenCache.get_token_for(@test_audience)
   end
 
   test "encrypts tokens" do
-    :ok = EncryptedRedisTokenCache.set_token_for("audience", sample_token())
+    :ok = EncryptedRedisTokenCache.set_token_for(@test_audience, sample_token())
 
-    persisted_token = Redix.command!(:redix, ["GET", token_key("audience")])
+    persisted_token = Redix.command!(:redix, ["GET", token_key(@test_audience)])
 
     assert is_binary(persisted_token)
     assert {:error, _} = Jason.decode(persisted_token)
@@ -35,14 +36,14 @@ defmodule Auth0Ex.TokenProvider.EncryptedRedisTokenCacheTest do
 
   test "tokens are deleted from cache when they expire" do
     token = %TokenInfo{sample_token() | expires_at: in_one_second()}
-    :ok = EncryptedRedisTokenCache.set_token_for("audience", token)
+    :ok = EncryptedRedisTokenCache.set_token_for(@test_audience, token)
 
-    assert {:ok, ^token} = EncryptedRedisTokenCache.get_token_for("audience")
+    assert {:ok, ^token} = EncryptedRedisTokenCache.get_token_for(@test_audience)
     :timer.sleep(1100)
-    assert {:ok, nil} = EncryptedRedisTokenCache.get_token_for("audience")
+    assert {:ok, nil} = EncryptedRedisTokenCache.get_token_for(@test_audience)
   end
 
   defp sample_token, do: %TokenInfo{jwt: "my-token", issued_at: one_hour_ago(), expires_at: in_one_hour()}
-  defp token_key(audience), do: "auth0ex_tokens:#{@namespace}:#{audience}"
+  defp token_key(audience), do: "auth0ex_tokens:#{@namespace}:#{@test_audience}"
   defp in_one_second, do: Timex.now() |> Timex.shift(seconds: 1) |> Timex.to_unix()
 end
