1b81608244350cc850ea173ca02dd3eaa088a76c
Matteo Busi <mbusi81@gmail.com>
2021-03-16 12:16:14 +0100

    Lower plug version requirement

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Auth0Ex.MixProject do
       {:jason, "~> 1.2.2"},
       {:joken, "~> 2.3.0"},
       {:joken_jwks, "~> 1.1.0"},
-      {:plug, "~> 1.11"},
+      {:plug, "~> 1.10"},
       {:redix, "~> 1.0.0"},
       {:telepoison, "~> 0.1.1"},
       {:timex, "~> 3.6"}
