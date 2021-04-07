51ee5839a168acef6bc0ee31a6b257881ca801c4
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-04-07 15:58:21 +0200

    Make claims of forged tokens overridable

--- lib/auth0_ex/local_token.ex
+++ lib/auth0_ex/local_token.ex
@@ -11,11 +11,35 @@ defmodule Auth0Ex.LocalToken do
   The signature will be generated locally, hence it will **not** be valid.
   For this reason, this function should be used for local development only.
   """
-  def forge(audience) do
+  @spec forge(String.t(), Keyword.t()) :: String.t()
+  def forge(audience, extra_claims \\ []) do
+    claims = merge_claims(default_claims(audience), extra_claims)
+
     Joken.generate_and_sign!(
       %{},
-      %{"aud" => audience},
+      claims,
       @signer
     )
   end
+
+  defp merge_claims(base_claims, extra_claims) do
+    extra_claims =
+      extra_claims
+      |> Enum.map(fn {key, value} -> {Atom.to_string(key), value} end)
+      |> Enum.into(%{})
+
+    Map.merge(base_claims, extra_claims)
+  end
+
+  defp default_claims(audience) do
+    %{"aud" => audience, "iat" => time_from_now(hours: -1), "exp" => time_from_now(hours: 1), "iss" => issuer()}
+  end
+
+  defp time_from_now(options) do
+    Timex.now()
+    |> Timex.shift(options)
+    |> Timex.to_unix()
+  end
+
+  defp issuer, do: Application.fetch_env!(:auth0_ex, :server)[:issuer]
 end
