1845aced9f54e29d28b4251e63d5651afc3b3690
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-04-13 11:01:38 +0200

    Add test config

--- config/test.exs
+++ config/test.exs
@@ -4,8 +4,10 @@ config :auth0_ex,
   authorization_service: AuthorizationServiceMock,
   refresh_strategy: RefreshStrategyMock,
   token_cache: TokenCacheMock,
-  token_service: TokenServiceMock
+  token_service: TokenServiceMock,
+  token_verifier: TokenVerifierMock
 
 config :auth0_ex, :client,
   cache_enabled: true,
-  token_check_interval: :timer.seconds(1)
+  token_check_interval: :timer.seconds(1),
+  signature_check_interval: :timer.seconds(1)
