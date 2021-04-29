f0b72e742aec4ff44c6e096832a0ba7f6e8a1830
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-04-29 10:32:26 +0200

    Handle retrieval of tokens cached by a previous version of auth0

--- test/integration/encrypted_redis_token_cache_test.exs
+++ test/integration/encrypted_redis_token_cache_test.exs
@@ -21,6 +21,16 @@ defmodule Integration.TokenProvider.EncryptedRedisTokenCacheTest do
     assert {:ok, token} == EncryptedRedisTokenCache.get_token_for(@test_audience)
   end
 
+  test "retrieves tokens set by a previous version of auth0_ex, hence without kid" do
+    issued_at = one_hour_ago()
+    expires_at = in_one_hour()
+    token_without_kid = %{jwt: "my-token", issued_at: issued_at, expires_at: expires_at}
+    :ok = EncryptedRedisTokenCache.set_token_for(@test_audience, token_without_kid)
+
+    assert {:ok, %{jwt: "my-token", issued_at: ^issued_at, expires_at: ^expires_at}} =
+             EncryptedRedisTokenCache.get_token_for(@test_audience)
+  end
+
   test "returns {:ok, nil} when token is not cached" do
     assert {:ok, nil} == EncryptedRedisTokenCache.get_token_for(@test_audience)
   end
@@ -51,7 +61,10 @@ defmodule Integration.TokenProvider.EncryptedRedisTokenCacheTest do
     assert {:ok, nil} = EncryptedRedisTokenCache.get_token_for(@test_audience)
   end
 
-  defp sample_token, do: %TokenInfo{jwt: "my-token", issued_at: one_hour_ago(), expires_at: in_one_hour()}
+  defp sample_token do
+    %TokenInfo{jwt: "my-token", issued_at: one_hour_ago(), expires_at: in_one_hour(), kid: "my-kid"}
+  end
+
   defp token_key(audience), do: "auth0ex_tokens:#{namespace()}:#{audience}"
   defp in_two_seconds, do: Timex.now() |> Timex.shift(seconds: 2) |> Timex.to_unix()
   defp namespace, do: Application.fetch_env!(:auth0_ex, :client)[:cache_namespace]
