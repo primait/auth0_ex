0ac89e5e3e5fa0aa7459a4cf594bf55ed402dac5
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-04-29 12:52:35 +0200

    No longer start JwksStrategy for client

--- lib/auth0_ex/application.ex
+++ lib/auth0_ex/application.ex
@@ -6,7 +6,7 @@ defmodule Auth0Ex.Application do
   alias Auth0Ex.{JwksStrategy, TokenProvider}
 
   def start(_type, _args) do
-    children = client_children() ++ cache_children() ++ jwks_strategy()
+    children = client_children() ++ cache_children() ++ server_children()
 
     opts = [strategy: :one_for_one, name: Auth0Ex.Supervisor]
     Supervisor.start_link(children, opts)
@@ -28,22 +28,17 @@ defmodule Auth0Ex.Application do
     end
   end
 
-  defp jwks_strategy do
-    children = [{JwksStrategy, [first_fetch_sync: first_jwks_fetch_sync()]}]
-
-    cond do
-      server_configured?() && not server_signature_ignored?() -> children
-      client_configured?() && not client_signature_ignored?() -> children
-      true -> []
+  defp server_children do
+    if server_configured?() && not server_signature_ignored?() do
+      [{JwksStrategy, [first_fetch_sync: first_jwks_fetch_sync()]}]
+    else
+      []
     end
   end
 
   defp cache_enabled?, do: Application.get_env(:auth0_ex, :client, cache_enabled: false)[:enabled]
   defp client_configured?, do: Application.get_env(:auth0_ex, :client) != nil
   defp server_configured?, do: Application.get_env(:auth0_ex, :server) != nil
-
-  defp client_signature_ignored?,
-    do: :auth0_ex |> Application.get_env(:client, []) |> Keyword.get(:ignore_signature, false)
 
   defp server_signature_ignored?,
     do: :auth0_ex |> Application.get_env(:server, []) |> Keyword.get(:ignore_signature, false)
