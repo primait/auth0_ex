f18cf427bfa541e6767a3d5d523b401b124e243b
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-05-20 18:12:57 +0200

    refactor

--- lib/prima_auth0_ex/middleware/require_permissions.ex
+++ lib/prima_auth0_ex/middleware/require_permissions.ex
@@ -5,40 +5,35 @@ defmodule PrimaAuth0Ex.Middleware.RequirePermissions do
 
   require Logger
 
-  alias Absinthe.Resolution
   alias PrimaAuth0Ex.Plug.CreateSecurityContext.Context
 
   @behaviour Absinthe.Middleware
 
   @impl true
   def call(
-        %{context: %Context{dry_run: dry_run, permissions: permissions}} = resolution,
+        %{context: %Context{permissions: permissions}} = resolution,
         required_permissions
       ) do
-    permissions
-    |> has_required_permissions?(required_permissions)
-    |> then(fn has_required_permissions? ->
-      # On dry-run, log a warning if token doesn't have required permission
-      # but permissions was not nil (i.e. it was supposed to work)
-      if not has_required_permissions? and permissions != nil and dry_run do
-        Logger.warn("Received invalid token", required_permissions: required_permissions)
-      end
-
-      has_required_permissions?
-    end)
-    |> resolve(dry_run, resolution)
+    if has_required_permissions?(permissions, required_permissions) do
+      resolution
+    else
+      resolve(resolution, required_permissions)
+    end
   end
 
-  @spec has_required_permissions?(permissions :: [any()] | nil, required_permissions :: [any()] | nil) :: boolean()
-  defp has_required_permissions?(nil, _), do: false
-  defp has_required_permissions?(_, nil), do: false
+  defp has_required_permissions?(nil = _permissions, _required_permissions), do: false
 
   defp has_required_permissions?(permissions, required_permissions),
     do: Enum.all?(required_permissions, fn required_permission -> required_permission in permissions end)
 
-  @spec resolve(has_required_permission? :: boolean(), dry_run :: boolean(), resolution :: Resolution.t()) ::
-          Resolution.t()
-  defp resolve(true, _, resolution), do: resolution
-  defp resolve(false, true, resolution), do: resolution
-  defp resolve(false, false, resolution), do: Absinthe.Resolution.put_result(resolution, {:error, "unauthorized"})
+  defp resolve(%{context: %Context{dry_run: true, permissions: permissions}} = resolution, required_permissions) do
+    if permissions != nil do
+      Logger.warn("Received invalid token", required_permissions: required_permissions)
+    end
+
+    resolution
+  end
+
+  defp resolve(%{context: %Context{dry_run: false}} = resolution, _required_permissions),
+    do: Absinthe.Resolution.put_result(resolution, {:error, "unauthorized"})
 end
