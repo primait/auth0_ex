ad452b9b583377237353f3fb9ea274f768c8a630
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-05-30 17:19:28 +0200

    fix credo

--- lib/prima_auth0_ex/telemetry.ex
+++ lib/prima_auth0_ex/telemetry.ex
@@ -4,6 +4,8 @@ if Code.ensure_loaded?(Statix) do
     A pre-defined Statix module
     """
 
+    alias PrimaAuth0Ex.Telemetry.Handler
+
     use Statix, runtime_config: true
 
     def setup do
@@ -14,11 +16,11 @@ if Code.ensure_loaded?(Statix) do
             [:prima_auth0_ex, :retrieve_token, :failure],
             [:prima_auth0_ex, :retrieve_token, :success]
           ],
-          &PrimaAuth0Ex.Telemetry.Handler.handle_event/4,
+          &Handler.handle_event/4,
           nil
         )
 
-      :ok = PrimaAuth0Ex.Telemetry.Statix.connect()
+      :ok = __MODULE__.connect()
     end
   end
 
