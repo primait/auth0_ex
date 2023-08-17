d4ba66f479a4f27a5fe947fa1bb869a76318c299
Cristiano Piemontese <cristiano.piemontese@prima.it>
2023-08-17 14:14:13 +0200

    account for single client config

--- lib/prima_auth0_ex/application.ex
+++ lib/prima_auth0_ex/application.ex
@@ -20,33 +20,41 @@ defmodule PrimaAuth0Ex.Application do
   defp client_children do
     clients = Application.get_env(:prima_auth0_ex, :clients, [])
 
-    Enum.reduce(clients, [], fn client_name, acc ->
-      if client_configured?(client_name) do
-        [
-          {TokenProvider, credentials: PrimaAuth0Ex.Auth0Credentials.from_env(client_name), name: client_name}
-          | acc
-        ]
-      else
-        acc
-      end
-    end)
+    if Enum.empty?(clients) and client_configured?() do
+      [{TokenProvider, credentials: PrimaAuth0Ex.Auth0Credentials.from_env(), name: TokenProvider}]
+    else
+      Enum.reduce(clients, [], fn client_name, acc ->
+        if client_configured?(client_name) do
+          [
+            {TokenProvider, credentials: PrimaAuth0Ex.Auth0Credentials.from_env(client_name), name: client_name}
+            | acc
+          ]
+        else
+          acc
+        end
+      end)
+    end
   end
 
   defp cache_children do
     clients = Application.get_env(:prima_auth0_ex, :clients, [])
 
-    Enum.reduce(clients, [], fn client_name, acc ->
-      if cache_enabled?(client_name) do
-        [
-          {Redix,
-           {redis_connection_uri(client_name),
-            [name: String.to_atom("#{client_name}_redix")] ++ redis_ssl_opts(client_name)}}
-          | acc
-        ]
-      else
-        acc
-      end
-    end)
+    if Enum.empty?(clients) and cache_enabled?() do
+      [{Redix, {redis_connection_uri(), [name: PrimaAuth0Ex.Redix] ++ redis_ssl_opts()}}]
+    else
+      Enum.reduce(clients, [], fn client_name, acc ->
+        if cache_enabled?(client_name) do
+          [
+            {Redix,
+             {redis_connection_uri(client_name),
+              [name: String.to_atom("#{client_name}_redix")] ++ redis_ssl_opts(client_name)}}
+            | acc
+          ]
+        else
+          acc
+        end
+      end)
+    end
   end
 
   defp server_children do
@@ -57,10 +65,10 @@ defmodule PrimaAuth0Ex.Application do
     end
   end
 
-  defp cache_enabled?(client_name),
+  defp cache_enabled?(client_name \\ :client),
     do: Application.get_env(:prima_auth0_ex, client_name, cache_enabled: false)[:cache_enabled]
 
-  defp client_configured?(client_name),
+  defp client_configured?(client_name \\ :client),
     do: Application.get_env(:prima_auth0_ex, client_name) != nil
 
   defp server_configured?, do: Application.get_env(:prima_auth0_ex, :server) != nil
@@ -68,13 +76,13 @@ defmodule PrimaAuth0Ex.Application do
   defp server_signature_ignored?,
     do: :prima_auth0_ex |> Application.get_env(:server, []) |> Keyword.get(:ignore_signature, false)
 
-  defp redis_connection_uri(client_name),
+  defp redis_connection_uri(client_name \\ :client),
     do:
       :prima_auth0_ex
       |> Application.fetch_env!(client_name)
       |> Keyword.get(:redis_connection_uri)
 
-  def redis_ssl_opts(client_name) do
+  def redis_ssl_opts(client_name \\ :client) do
     if redis_ssl_enabled?(client_name) do
       append_if([ssl: true], redis_ssl_allow_wildcard_certificates?(client_name),
         socket_opts: [
