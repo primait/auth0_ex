1a0a569bce1721e3d5d6e12b12383f037e3b2829
Cristiano Piemontese <cristiano.piemontese@prima.it>
2023-08-23 10:26:12 +0200

    fix formatting yet again

--- lib/prima_auth0_ex/application.ex
+++ lib/prima_auth0_ex/application.ex
@@ -24,8 +24,7 @@ defmodule PrimaAuth0Ex.Application do
   defp client_children do
     if client_configured?() do
       [
-        {TokenProvider,
-         credentials: PrimaAuth0Ex.Auth0Credentials.from_env(), name: TokenProvider}
+        {TokenProvider, credentials: PrimaAuth0Ex.Auth0Credentials.from_env(), name: TokenProvider}
       ]
     end
 
@@ -35,8 +34,7 @@ defmodule PrimaAuth0Ex.Application do
       |> Enum.reduce([], fn client_name, acc ->
         [
           Supervisor.child_spec(
-            {TokenProvider,
-             credentials: PrimaAuth0Ex.Auth0Credentials.from_env(client_name), name: client_name},
+            {TokenProvider, credentials: PrimaAuth0Ex.Auth0Credentials.from_env(client_name), name: client_name},
             id: client_name
           )
           | acc
