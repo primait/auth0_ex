5cc140bb955c0fd9f4979a04706d523026c916b9
mae.kasza <26093674+MaeIsBad@users.noreply.github.com>
2023-11-02 11:45:34 +0100

    Fetch jwks_uri from openid-configuration

--- lib/prima_auth0_ex/jwks_strategy.ex
+++ lib/prima_auth0_ex/jwks_strategy.ex
@@ -11,7 +11,15 @@ defmodule PrimaAuth0Ex.JwksStrategy do
     Keyword.merge(opts, jwks_url: jwks_url())
   end
 
-  defp jwks_url, do: base_url() <> "/.well-known/jwks.json"
+  defp jwks_url do
+    %HTTPoison.Response{status_code: status_code, body: meta_body} = Telepoison.get!(oidc_metadata_url())
+    true = status_code in 200..299
+
+    meta_body
+    |> Jason.decode!()
+    |> Map.fetch!("jwks_uri")
+  end
 
   defp base_url, do: Config.server!(:auth0_base_url)
+  defp oidc_metadata_url, do: base_url() <> "/.well-known/openid-configuration"
 end
