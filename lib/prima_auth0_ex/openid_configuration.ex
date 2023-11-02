922eed9fafda6a26961b1714eb66c9e22a1731a4
mae.kasza <26093674+MaeIsBad@users.noreply.github.com>
2023-11-02 17:15:02 +0100

    Add docs to the OpenIDConfiguration module

--- lib/prima_auth0_ex/openid_configuration.ex
+++ lib/prima_auth0_ex/openid_configuration.ex
@@ -1,14 +1,24 @@
 defmodule PrimaAuth0Ex.OpenIDConfiguration do
-  @moduledoc "Credentials to access Auth0"
+  @moduledoc false
+  @doc """
+    Module for fetching and parsing the [rfc8414](https://www.rfc-editor.org/rfc/rfc8414) openid metadata endpoint
+
+    This allows auth0_ex to be agnostic of the actual openid server
+  """
 
   @type t :: %__MODULE__{issuer: String.t(), token_endpoint: String.t(), jwks_uri: String.t()}
 
   @struct_keys [:issuer, :token_endpoint, :jwks_uri]
   defstruct @struct_keys
 
+  @doc """
+    Fetches the openid metadata.
+
+    Doesn't implement caching and always makes a http request, avoid calling this in hot code paths
+  """
   @spec fetch(String.t()) :: __MODULE__.t()
   def fetch(base_url) do
-    url = oidc_metadata_url(base_url)
+    url = metadata_url(base_url)
     %HTTPoison.Response{status_code: status_code, body: meta_body} = Telepoison.get!(url, accept: "application/json")
 
     true = status_code in 200..299
@@ -19,5 +29,5 @@ defmodule PrimaAuth0Ex.OpenIDConfiguration do
     struct!(__MODULE__, metadata)
   end
 
-  defp oidc_metadata_url(base_url), do: base_url <> "/.well-known/openid-configuration"
+  defp metadata_url(base_url), do: base_url <> "/.well-known/openid-configuration"
 end
