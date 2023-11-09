6b4f0b6392aa150d68530ddc0736c00b5b7b4ae4
mae.kasza <26093674+MaeIsBad@users.noreply.github.com>
2023-11-09 20:28:02 +0100

    Document the token_cache option

--- lib/prima_auth0_ex/config.ex
+++ lib/prima_auth0_ex/config.ex
@@ -34,6 +34,11 @@ defmodule PrimaAuth0Ex.Config do
   def server!(prop), do: fetch_env!(:server, prop)
   def telemetry_reporter, do: get_env(:telemetry_reporter, nil)
 
+  @doc """
+  Module to use for caching tokens.
+  Needs to implement the PrimaAuth0Ex.TokenCache behavior.
+  Modules with the PrimaAuth0Ex.TokenCache prefix(PrimaAuth0Ex.TokenCache.EncryptedRedisTokenCache, PrimaAuth0Ex.TokenCache.NoopCache) can be used without the prefix(just EncryptedRedisTokenCache and NoopCache)
+  """
   def token_cache(default), do: get_env(:token_cache, default)
 
   def token_service(default), do: get_env(:token_service, default)
